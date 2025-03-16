a0f22d5287d46b3faae07fd160daa1ba337cc6a4
Momen Marwan <momenmarwanh@gmail.com>
2025-03-16 11:53:08 +0200

    first

---
+++ src/test/java/ppu/momen/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package ppu.momen.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
